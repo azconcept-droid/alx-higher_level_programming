0079fb689b782f733028d918bf7cb93c96f87156
Yahaya Azeez <azconcept2016@gmail.com>
2022-06-01 22:40:59 +0000

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-for i in range(65, 123):
-    print("{:c}".format(i), end="")
+i = 122
+while i > 96:
+    if i % 2 == 0:
+        print("{:s}".format(chr(i)), end="")
+    elif i % 2 != 0:
+        capLetter = chr(i - 32)
+        print("{:s}".format(capLetter), end="")
+    i -= 1
