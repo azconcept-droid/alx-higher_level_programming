4b795845f3435ce2ce2fc329fdf1951599c3264d
Yahaya Azeez Adebayo <azconcept2016@gmail.com>
2022-06-02 00:21:09 +0100

    Delete 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-i = 122
-while i > 96:
-    if i % 2 == 0:
-        print("{:s}".format(chr(i)), end="")
-    elif i % 2 != 0:
-        print("{:s}".format(chr(i - 32)), end="")
-    i -= 1
