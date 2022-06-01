0ec7d03160325a6ad711b2a6ecc7cb6ac586e0ab
Yahaya Azeez <azconcept2016@gmail.com>
2022-06-01 23:33:16 +0000

    100-print_tebahpla.py

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+i = 122
+while i > 96:
+    if i % 2 == 0:
+        print('{:s}'.format(chr(i)), end="")
+    else:
+        print('{:s}'.format(chr(i - 32)), end="")
+    i -= 1
