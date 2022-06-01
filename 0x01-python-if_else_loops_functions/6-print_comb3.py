648581a53aa21a3e6cd7b2abb6187b2edb485b26
Yahaya Azeez <azconcept2016@gmail.com>
2022-06-01 05:02:51 -0700

    ADD 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i % 11 == 0 or i % 10 == 0 or i > 90 or i == 21:
-        continue
-    if (i > 80 and i < 89) or (i > 70 and i < 77):
-        continue
-    if (i > 60 and i < 67) or (i > 50 and i < 57):
-        continue
-    if (i > 40 and i < 44) or (i > 30 and i < 34):
-        continue
-    if i == 89:
-        break
-    print('{:02d}'.format(i), end=", ")
-print("89")
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        if i < j and i != j:
+            print("{}{}".format(i, j), end=", ")
