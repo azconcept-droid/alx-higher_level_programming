ee078206f7cc6ccf9ca31f9d679fe9d85ca99d7a
Yahaya Azeez <azconcept2016@gmail.com>
2022-05-31 21:14:43 -0700

    101-remove_char_at.py

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    while i <= len(str):
+        i += 1
+        if i == n:
+            continue
+        str_new[i] = str[i]
+    print("{}".format(str_new)
