2ca0b1b908ef878476ffb4983fd462f6c0b451dd
Yahaya Azeez <azconcept2016@gmail.com>
2022-06-01 06:08:51 -0700

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,17 +2,21 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-if number < 0:
-    last_digit = (-1 * number) % 10
-    if last_digit == 0:
-        print('Last digit of {:d} is {:d} and is 0'.format(number, last_digit))
-    elif last_digit < 6 and last_digit != 0:
-        print('Last digit of {:d} is -{:d} and is less than 6 and not 0'.format(number, last_digit))
-if number > 0:
-    last_digit = number % 10
-    if last_digit == 0:
-        print('Last digit of {:d} is {:d} and is 0'.format(number, last_digit))
-    elif last_digit > 5:
-        print('Last digit of {:d} is {:d} and is greater than 5'.format(number, last_digit))
-    elif last_digit < 6 and last_digit != 0:
-        print('Last digit of {:d} is {:d} and is less than 6 and not 0'.format(number, last_digit))
+n = number
+if n < 0:
+    l = (-1 * number) % 10
+    if l == 0:
+        print('Last digit of {:d} is {:d} and is 0'.format(n, l))
+    elif l < 6 and l != 0:
+        print('Last digit of {: d} is -{} and is less than 6 and not 0'.format(n, l))
+elif n > 0:
+    l = n % 10
+    if l == 0:
+        print('Last digit of {:d} is {:d} and is 0'.format(n, l))
+    elif l > 5:
+        print('Last digit of {:d} is {} and is greater than 5'.format(n, l))
+    elif l < 6 and l != 0:
+        print('Last digit of {:d} is {} and is less than 6 and not 0'.format(n, l))
+elif n == 0:
+    l = 0
+    print('Last digit of {:d} is {} and is 0'.format(n, l))
