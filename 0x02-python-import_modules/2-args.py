884725af67da1110e52e13774da3caacb1692487
Yahaya Azeez <azconcept2016@gmail.com>
2022-06-02 08:09:42 -0700

    Add code

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+if __name__ == "__main__":
+    import sys
+    for i in range(len(sys.argv)):
+        match i:
+            case 0:
+                print("0 arguments.")
+
+
