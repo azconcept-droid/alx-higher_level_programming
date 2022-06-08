ae731354b2d467408e5c2b05071d81933c20f1b5
Yahaya Azeez <azconcept2016@gmail.com>
2022-06-08 16:45:42 +0000

    :duck: Modify 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    i = 0
+    k = 1
     for row in matrix:
         for col in row:
-            print(" {:d}".format(col), end="")
+            i += 1
+            if i == 3 * k:
+                print("{:d}".format(col), end="")
+                k += 1
+            else:
+                print("{:d}".format(col), end=" ")
         print()
