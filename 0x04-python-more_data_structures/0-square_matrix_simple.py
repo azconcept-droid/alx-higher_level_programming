a0eee63caa26ec09fd9c2b66873a92c0c2cb1aa6
Yahaya Azeez <azconcept2016@gmail.com>
2022-06-08 15:37:00 -0700

    :duck: Add code

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    def num_square(num):
+        return num ** 2
 
+    new_matrix = matrix[:]
+    for row in matrix:
+        new_matrix = list(map(lambda x : x ** 2, row))
+    return new_matrix
