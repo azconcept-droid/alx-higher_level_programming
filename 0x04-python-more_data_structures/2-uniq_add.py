e9ebeb627425002bd639cd0dd3138c994b1f7f14
Yahaya Azeez <azconcept2016@gmail.com>
2022-06-08 17:36:12 -0700

    Add code

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-
+    uniq_list = list(set(my_list))
+    add = 0
+    for i in range(len(uniq_list)):
+        add = add + uniq_list[i]
+    return add
