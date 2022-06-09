3eb117b700059c6f8753bd255d3388d12ab3f6ad
Yahaya Azeez <azconcept2016@gmail.com>
2022-06-08 17:04:05 -0700

    Add code

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-
+    new_list = []
+    j = 0
+    for i in my_list:
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(my_list[j])
+        j += 1
+    return new_list
