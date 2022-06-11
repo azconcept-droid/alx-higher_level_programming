033ba85877100935af124285f25cb0a568b32028
Yahaya Azeez <azconcept2016@gmail.com>
2022-06-11 19:48:45 +0000

    Interseting :smile:

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+new_matrix = []
 def square_matrix_simple(matrix=[]):
-
+    for row in matrix:
+        col = []
+        for integers in row:
+            col.append(integers ** 2)
+        new_matrix.append(col)
+    return new_matrix
