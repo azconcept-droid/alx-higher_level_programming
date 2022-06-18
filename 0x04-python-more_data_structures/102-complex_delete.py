390fcd729584a7f3a2e0085d786159811e65d99c
azconcept-droid <azconcept2016@gmail.com>
2022-06-18 02:06:34 +0100

    Correct indent

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-for k, v in a_dictionary.items():
-		if v == value:
-			del a_dictionary[k]
+    for k, v in a_dictionary.items():
+        if v == value:
+            a_dictionary.pop(k)
