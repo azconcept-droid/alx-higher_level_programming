507205f83d967b7572f639e899c2fad9d5e4cf4a
Yahaya <azconcept2016@gmail.com>
2022-06-23 02:47:15 +0100

    Task 1 commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    """Prints x elements of a list"""
+
+    counter = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            counter += 1
+        except IndexError:
+            break
+    print()
+    return counter
