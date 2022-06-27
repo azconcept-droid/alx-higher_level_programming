a2bd6e065b038bb40c746b8f269080483733bb97
Yahaya <azconcept2016@gmail.com>
2022-06-28 00:48:08 +0100

    :bug: bug fix line 75

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -6,7 +6,7 @@ class Rectangle:
     """A rectangle object"""
 
     number_of_instances = 0
-    print_symbol = '#' 
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         self.width = width
@@ -72,10 +72,12 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
 
     def bigger_or_equal(rect_1, rect_2):
-        if not instance(rect_1, Rectangle):
+        if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if not instance(rect_2, Rectangle):
+        if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() == rect_2.area():
+
+        if rect_1.area() >= rect_2.area():
             return rect_1
-        if rect_1.
+        else:
+            return rect_2
