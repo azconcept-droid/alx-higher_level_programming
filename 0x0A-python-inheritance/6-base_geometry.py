a01391bcdfdc0e07cc58306006aaf14f5ee5265f
Yahaya <azconcept2016@gmail.com>
2022-07-04 22:30:47 +0100

    :white_check_mark: public instance method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""An empty class that defines a Geometry"""
+
+
+class BaseGeometry:
+    """A BaseGeometry object"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
