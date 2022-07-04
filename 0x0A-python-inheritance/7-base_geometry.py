3ba6c0d95854d9f8c0943691f5e347adc87aae45
Yahaya <azconcept2016@gmail.com>
2022-07-04 22:45:09 +0100

    :white_check_mark: public instance method that validates value

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""An empty class that defines a Geometry"""
+
+
+class BaseGeometry:
+    """A BaseGeometry object"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
