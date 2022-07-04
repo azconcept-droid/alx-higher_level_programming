a45ed4352287f2f49213dbcd594a0e01662ba822
Yahaya <azconcept2016@gmail.com>
2022-07-04 23:23:17 +0100

    :white_check_mark: initilize new rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,39 +1,18 @@
 #!/usr/bin/python3
-"""An empty class that defines a Geometry"""
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry:
-    """A BaseGeometry object"""
+class Rectangle(BaseGeometry):
+    """Represent a rectangle inherited from BaseGeometry."""
 
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate data
-
+    def __init__(self, width, height):
+        """Intialize a new Rectangle.
         Args:
-            name (str): name of the geometry
-            value (int): value of the the geometry
-        Raise:
-            TypeError: if value is not an integer
-            ValueError: if value is less or equal to 0
+            width (int): width of the Rectangle.
+            height (int): height of the Rectangle.
         """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-    class Rectangle(BaseGeometry):
-        """Rectangle object"""
-
-        def __init__(self, width, height):
-            """initialize rectangle class
-
-            Args:
-                width (int): width of the geometry
-                height (int): height of the the geometry
-            """
-            integer_validator(name, width)
-            integer_validator(name, height)
-            self.__width = width
-            self.__height = height
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
