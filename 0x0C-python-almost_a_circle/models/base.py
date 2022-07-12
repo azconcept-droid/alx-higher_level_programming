6a2c56b5902dc0131e5068518e82079896c5a0a8
Yahaya <azconcept2016@gmail.com>
2022-07-13 00:19:29 +0100

    :memo: Update documentation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Docs"""
+"""Base class module"""
 import json
+
 
 class Base:
     """Docs"""
@@ -8,18 +9,32 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id != None:
+        """Constructor
+        Args:
+            id (int): identifier
+        """
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        """Returns the JSON string representation
+        of list_dictionaries
+        Agrs:
+            list_dictionaries (list): list dictionaries of parameter
+        """
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
 
     def from_json_string(json_string):
+        """Returns the list of the JSON string representation
+        json_string
+        Args:
+            json_string (str): json string of a list of dictionaries
+        """
         if json_string is None:
             return "[]"
         return json.loads(json_string)
