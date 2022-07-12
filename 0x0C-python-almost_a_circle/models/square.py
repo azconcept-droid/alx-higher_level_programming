a349cab921047a4b2e810b05ac6d534375ec87c3
Yahaya <azconcept2016@gmail.com>
2022-07-12 10:03:19 +0100

    :poop:

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square class module"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class Square that inherits from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Docs"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return '[{}] ({}) {}/{} - {}' \
+        .format(type(self.__name__), self.id, self.)
