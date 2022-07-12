3bf877cae7e498d64d1cc94b95f44f3071dbe6b6
Yahaya <azconcept2016@gmail.com>
2022-07-13 00:31:58 +0100

    :art: Update base.py with from_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,14 +4,18 @@ import json
 
 
 class Base:
-    """Docs"""
+    """Represent the base model.
+    Represents the "base" for all other classes in project 0x0C*.
+    Attributes:
+        __nb_objects (int): The number of instantiated Bases.
+    """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Constructor
+        """Initialize the base class
         Args:
-            id (int): identifier
+            id (int): identity of the base class
         """
         if id is not None:
             self.id = id
@@ -19,22 +23,24 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation
         of list_dictionaries
         Agrs:
             list_dictionaries (list): list dictionaries of parameter
         """
-        if list_dictionaries is None:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    @staticmethod
     def from_json_string(json_string):
         """Returns the list of the JSON string representation
         json_string
         Args:
             json_string (str): json string of a list of dictionaries
         """
-        if json_string is None:
+        if json_string is None or json_string == []:
             return "[]"
         return json.loads(json_string)
