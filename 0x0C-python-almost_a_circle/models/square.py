8ab02a35893fa982fdb913170c0a4c31ed0b2422
Yahaya <azconcept2016@gmail.com>
2022-07-12 18:41:29 +0100

    :construction: working progress

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,6 +11,44 @@ class Square(Rectangle):
         """Docs"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Get size of the square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set size of the square.
+        Args:
+            value (int): value of size
+        """
+        self.integers_validator("width", value)
+        self.__width = value
+        self.__height = value
+
     def __str__(self):
         return '[{}] ({}) {}/{} - {}' \
         .format(type(self).__name__, self.id, self.x, self.y, self.width)
+
+    def help_update(self, id=None, size=None, x=None, y=None):
+        """Internal method that help update method"""
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.__size = size
+        if x is not None:
+            self.__x = x
+        if y is not None:
+            self.__y = y
+
+    def update(self, *args, **kargs):
+        """Update arguments
+
+        Args:
+            *args (non-keyworded): 
+            **kargs (keyworded):
+        """
+        if args:
+            self.help_update(*args)
+        else:
+            self.help_update(**kargs)
