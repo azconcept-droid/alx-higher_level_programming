876388988bdad96b66737c905e79e96b5ab81152
Yahaya <azconcept2016@gmail.com>
2022-07-12 23:35:31 +0100

    :art: Update base.py with to_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Docs"""
+import json
 
 class Base:
     """Docs"""
@@ -12,3 +13,8 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
