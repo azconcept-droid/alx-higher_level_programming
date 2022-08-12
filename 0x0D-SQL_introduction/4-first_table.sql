534c7974eec829b5cadc0eff253062571187b62f
Yahaya <azconcept2016@gmail.com>
2022-08-12 11:36:35 +0100

    :bulb: Update comment

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
---  Creates a table called first_table with values id and name
+-- Creates a table called first_table with values id and name.
 CREATE TABLE IF NOT EXITS `first_table` (`id` INT, `name` VARCHAR(256));
