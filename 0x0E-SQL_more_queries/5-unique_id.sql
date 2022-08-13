61abbba7c00f401ff13afe0436f2e5b11e3a3f4f
Yahaya <azconcept2016@gmail.com>
2022-08-13 12:43:46 +0100

    :sparkles:add script to file

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
---  Creates the table unique_id.
-CREATE TABLE IF NOT EXITS `unique_id`(
+-- Creates the table unique_id.
+CREATE TABLE IF NOT EXITS `unique_id` (
     `id` INT DEFAULT 1 UNIQUE,
     `name` VARCHAR(256)
 );
