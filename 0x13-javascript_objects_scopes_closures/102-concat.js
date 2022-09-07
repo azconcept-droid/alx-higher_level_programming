f5eb3e277caabc98a91e7b5f0c412ffb3deaa07f
Yahaya <azconcept2016@gmail.com>
2022-09-08 00:32:38 +0100

    feat: script concats 2 files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+/**
+ * This script concats 2 files
+ */
+const fs = require('fs');
+
+const args = process.argv.slice(2);
+const [file1, file2, toFile] = args;
+
+if (!file1 || !file2 || !toFile) {
+  process.exit(0);
+}
+
+const content1 = fs.readFileSync(file1).toString();
+const content2 = fs.readFileSync(file2).toString();
+fs.writeFileSync(toFile, content1 + content2);
