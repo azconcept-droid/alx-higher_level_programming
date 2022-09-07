d1d3e18adb7e4c2acd4698f825994e87ce265482
Yahaya <azconcept2016@gmail.com>
2022-09-07 17:14:29 +0100

    feat: add if statement

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/**
+ * This script create a class Rectangle
+ * that defines a rectangle.
+ * If w or h is equal to 0 or not a positive integer,
+ * create an empty object.
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
