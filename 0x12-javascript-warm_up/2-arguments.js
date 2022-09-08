d174904c226a47ae74192b599a252a846b5d7905
Yahaya <azconcept2016@gmail.com>
2022-09-08 04:49:00 +0100

    feat: remove docs

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// This script prints a message depending of the number of arguments passed
+
 if (process.argv.length === 2) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
