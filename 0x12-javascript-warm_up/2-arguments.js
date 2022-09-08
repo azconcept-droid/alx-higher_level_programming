bad0eeb82eaf41b2f75939c25e2232bcaaca041f
Yahaya <azconcept2016@gmail.com>
2022-09-08 04:56:50 +0100

    feat: add docs

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
-// This script prints a message depending of the number of arguments passed
+/**
+ * This script prints a message
+ * depending of the number of arguments passed
+ */
 
-const args = process.argv
+const args = process.argv;
 
 if (args.length === 2) {
   console.log('No argument');
