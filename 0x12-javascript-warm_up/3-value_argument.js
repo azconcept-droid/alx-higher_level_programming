4ff2d97cca6e9bac3aaa2f20cf6d8bed738911c8
Yahaya <azconcept2016@gmail.com>
2022-09-08 05:19:28 +0100

    feat: prints the first argument passed to it

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// This script prints the first argument passed to it
+
+const args = process.argv;
+
+let count = 0;
+while (args[count]) {
+  count++;  //count the number of arguments
+}
+
+if (count === 2) {
+  console.log('No argument');
+} else {
+  console.log(args[2]);
+}
