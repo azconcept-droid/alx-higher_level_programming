79e99e0ddbccd585a9121cdcdd25e184e5810446
Yahaya <azconcept2016@gmail.com>
2022-09-08 04:53:40 +0100

    feat: add docs and declare variable

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
+// This script prints a message depending of the number of arguments passed
 
-if (process.argv.length === 2) {
+const args = process.argv
+
+if (args.length === 2) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
+} else if (args.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Argments found');
