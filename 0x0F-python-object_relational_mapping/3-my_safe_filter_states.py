67aa92bc1287029089561ead4ffe4e5f707559c7
Yahaya <azconcept2016@gmail.com>
2022-09-13 15:40:19 +0100

    [feature]: takes in argument and prevent sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This script takes in an argument and
+displays all values in the states table
+and write out the safe form
+that is safe from MySQL injections!
+"""
+
+import MySQLdb as mysql
+from sys import argv
+
+name = argv[4]
+
+if __name__ == "__main__":
+    # Connecting to database and getting database object
+    mydb = mysql.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    # Creating a cursor object for multiple connections
+    cur = mydb.cursor()
+    # Pass command to mysql
+    cur.execute("SELECT * FROM states \
+                WHERE states.name=%(name_state)s\
+                ORDER BY states.id ASC", {"name_state": name})
+    # Fetch all results of executed queries
+    rows = cur.fetchall()
+    # Print out each rows line by line
+    for row in rows:
+        print(row)
+
+    # Disconnect cursor and close database
+    cur.close()
+    mydb.close()
