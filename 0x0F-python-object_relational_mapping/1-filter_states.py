fd8dae211c82e7f61ef8299fcd19d21fed3270bc
Yahaya <azconcept2016@gmail.com>
2022-09-13 14:29:41 +0100

    [feature]: lists all states with a name

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This script lists all states with a name
+starting with N (upper N) from the database hbtn_0e_0_usa
+
+"""
+
+import MySQLdb as mysql
+from sys import argv
+
+if __name__ == "__main__":
+    # Connecting to database and getting database object
+    mydb = mysql.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    # Creating a cursor object for multiple connections
+    cur = mydb.cursor()
+    # Pass command to mysql
+    command = cur.execute("SELECT ALL FROM states \
+                            WHERE CONVERT(`name` USING Latin1)\
+                            COLLATE Latin1_General_CS\
+                            LIKE 'N%';")
+    #Fetch all results of executed queries
+    rows = command.fetchall()
+    # Print out each rows line by line
+    for row in rows:
+        print(row)
+
+    # Disconnect cursor and close database
+    cur.close()
+    mydb.close()
