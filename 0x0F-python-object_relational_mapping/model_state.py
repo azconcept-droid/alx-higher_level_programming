38a2e787631d8cf6d8b0c509eb0e02c83dd6cc66
Yahaya <azconcept2016@gmail.com>
2022-09-15 14:45:08 +0100

    feat: add State table class for sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module defines a class State"""
+
+import MySQLdb
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    State table class
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(128))
