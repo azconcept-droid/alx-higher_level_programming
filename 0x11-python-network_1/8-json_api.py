0e34d0922a1374fda2a977d41e53786bf4a82d5b
Yahaya <azconcept2016@gmail.com>
2022-09-24 08:40:13 +0100

    bux: just for pushing sake

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+This script takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter
+displays the body of the response(decoded in utf-8)
+"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.post(url, data={'email': sys.argv[2]}) as r:
+        print('Your email is: {}'.format(r.getheader('email')))
