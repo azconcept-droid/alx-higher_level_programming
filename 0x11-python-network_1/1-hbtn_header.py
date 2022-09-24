714093bcc1fcf098ab0abdc5d74446795390ff6c
Yahaya <azconcept2016@gmail.com>
2022-09-24 06:58:45 +0100

    feat: print X-Request-Id variable found in the header of the response.

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+This script takes in a URL, sends a request to the URL and
+displays the value of the X-Request-Id variable
+found in the header of the response.
+"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as r:
+        value = r.getheaders()
+    value_dict = dict(value)
+    print(value_dict['X-Request-Id'])
