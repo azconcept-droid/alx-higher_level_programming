6a962e98bd4d20ca32d7c291f3675dc7e3ced206
Yahaya <azconcept2016@gmail.com>
2022-09-24 15:28:10 +0100

    refactor: refactor code to print the body of variable pass

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,10 +5,15 @@ sends a POST request to the passed URL with the email as a parameter
 displays the body of the response(decoded in utf-8)
 """
 import urllib.request
+import urllib.parse
 import sys
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    with urllib.request.post(url, data={'email': sys.argv[2]}) as r:
-        print('Your email is: {}'.format(r.getheader('email')))
+    email = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(email)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as r:
+        print(r.read())
