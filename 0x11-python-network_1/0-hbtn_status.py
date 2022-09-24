55bb6e8049e6263ec7b2e62f02196e1656bac961
Yahaya <azconcept2016@gmail.com>
2022-09-24 05:27:58 +0100

    feat: fetches URL status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+This script fetches https://alx-intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as r:
+        html = r.read()
+        print('Body response:\n',
+            '   {} type: {}\n'.format('-', type(html)),
+            '   {} content: {}\n'.format('-', html),
+            '   {} utf8 content: {}'.format('-', r.reason))
