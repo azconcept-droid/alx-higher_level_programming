844e845c3c3f02b28d0f06142d8040c063bde800
Yahaya <azconcept2016@gmail.com>
2022-09-24 07:34:33 +0100

    feat: print email value

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,10 +4,12 @@ This script takes in a URL and an email,
 sends a POST request to the passed URL with the email as a parameter
 displays the body of the response(decoded in utf-8)
 """
-import urllib
+import urllib.request
 import sys
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    with urllib.request.
+    with urllib.request.urlopen(url) as r:
+        email = r.post(url, data={'email': sys.argv[2]})
+    print('Your email is: {}'.format(r.getheader('email')))
