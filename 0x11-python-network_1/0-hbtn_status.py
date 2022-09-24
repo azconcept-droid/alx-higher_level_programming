4f9db9ffbecbe8d6096d0a9beb67dce6a3d4dab6
Yahaya <azconcept2016@gmail.com>
2022-09-24 21:21:19 +0100

    feat: print each line sperately

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as r:
         html = r.read()
-    print('Body response:\n',
-          '{} type: {}\n'.format('-', type(html)),
-          '{} content: {}\n'.format('-', html),
-          '{} utf8 content: {}'.format('-', html.decode()))
+        print('Body response:')
+        print('\t{} type: {}'.format('-', type(html)))
+        print('\t{} content: {}'.format('-', html))
+        print('\t{} utf8 content: {}'.format('-', html.decode('utf-8')))
