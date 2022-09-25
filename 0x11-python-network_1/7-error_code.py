c86df88a2372e509574ac6d93f67b4aa36d37fd0
Yahaya <azconcept2016@gmail.com>
2022-09-25 08:28:24 +0100

    :bento: feature: display response body or HTTP status code error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 """
-This script takes in a URL and an email,
-sends a POST request to the passed URL with the email as a parameter
-displays the body of the response(decoded in utf-8)
+This script  takes in a URL, sends a request to the URL and
+displays the body of the response.
+If the HTTP status code is greater than or equal to 400,
+print: Error code: followed by the value of the HTTP status code
 """
-import urllib.request
+import requests
 import sys
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    with urllib.request.post(url, data={'email': sys.argv[2]}) as r:
-        print('Your email is: {}'.format(r.getheader('email')))
+    req = requests.get(url)
+    if req.status_code >= 400:
+        print('Error code:', req.status_code)
+    else:
+        print(req.text)
