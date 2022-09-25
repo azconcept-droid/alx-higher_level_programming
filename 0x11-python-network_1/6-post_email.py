74811231aba0886751d475313ec4669ac7380f6c
Yahaya <azconcept2016@gmail.com>
2022-09-25 08:10:27 +0100

    :bulb: docs: update documantation

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-This script  takes in a URL, sends a request to the URL and
-displays the value of the variable X-Request-Id in the response header
+This script takes in a URL, and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
 """
 import requests
 import sys
