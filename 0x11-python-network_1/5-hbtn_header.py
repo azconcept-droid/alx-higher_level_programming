1a79cb4e15ac217deb4ab94501705e7c24322167
Yahaya <azconcept2016@gmail.com>
2022-09-25 07:41:59 +0100

    feat: add if statement

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -10,4 +10,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     req = requests.get(url)
-    print(req.headers['X-Request-Id'])
+    if req.headers['X-Request-Id']:
+        print(req.headers['X-Request-Id'])
+    else:
+        print('', end="")
