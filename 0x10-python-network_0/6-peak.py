fe49358027dc60f074302fc63b9288a42153ebd4
Yahaya <azconcept2016@gmail.com>
2022-10-05 22:19:57 +0100

    docs: update documentations :bulb: and fix bugs :bug:

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-This module defines a function find_peak
+This module defines a function find_peak value of
+an unsorted list of integers
 """
 
 
 def find_peak_index(list_of_integers, Left=None, Right=None):
-    """Finds the peak element index of a list
+    """Finds the peak element index of a list using binary search algorithm
        list_of_integers (list) : list of integers
     """
 
@@ -15,20 +16,26 @@ def find_peak_index(list_of_integers, Left=None, Right=None):
 
     mid = (Left + Right) // 2  # middle index
 
+    # Check if the middle element is greater than its neighbors
     if ((mid == 0 or list_of_integers[mid - 1] <= list_of_integers[mid])
        and (mid == len(list_of_integers) - 1
        or list_of_integers[mid + 1] <= list_of_integers[mid])):
         return (mid)
 
+    # If the left neighbor of `mid` is greater than the middle element,
+    # find the peak recursively in the left sublist
     if mid - 1 >= 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
         return find_peak_index(list_of_integers, Left, mid - 1)
 
+    # If the right neighbor of `mid` is greater than the middle element,
+    # find the peak recursively in the right sublist
     return find_peak_index(list_of_integers, mid + 1, Right)
 
 
 def find_peak(list_of_integers):
     """Return peak element"""
 
+    # If list is empty return none
     if list_of_integers == []:
         return None
 
