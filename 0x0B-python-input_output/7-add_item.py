f89d805cd4fcc38fbc6030187d1a7481e1bfce22
Yahaya <azconcept2016@gmail.com>
2022-10-15 04:53:03 +0100

    feat: refactor code

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Module that adds all arguments to a Python list, and then save them to a file"""
+"""
+Module that adds all arguments to a Python list,
+and then save them to a file
+"""
 import sys
 
 if __name__ == '__main__':
@@ -7,8 +10,11 @@ if __name__ == '__main__':
     load_from_json_file = __import__(
         '6-load_from_json_file').load_from_json_file
 
-    if sys.argv[1:] == []:
-        save_to_json_file(sys.argv[1:], "add_item.json")
-    items = load_from_json_file("add_item.json")
+    items = []
+    try:
+        items = load_from_json_file("add_item.json")
+    except Exception:
+        save_to_json_file(items, "add_item.json")
+
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
