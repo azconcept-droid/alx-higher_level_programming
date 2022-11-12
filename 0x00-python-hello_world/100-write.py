98500fcffa312f86dc0e3dcd109d9f004b62c1d4
Yahaya <azconcept2016@gmail.com>
2022-11-13 00:44:45 +0100

    :tada: feat: add sys.stderr attribute

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import sys
-str = "and that piece of art is useful - Dora Korpar, 2015-10-19"
-write(stderr, str, len(str))
+str = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+sys.stderr.write(str)
