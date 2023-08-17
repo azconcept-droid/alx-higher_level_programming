8c333fdc19c601b60d1fdfbbe73502d9ff1ed768
Yahaya Azeez <azconcept2016@gmail.com>
2023-08-17 15:32:45 +0100

    fix invisible bug :bug:

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script displays all HTTP methods a server will accept.
-curl -isLX OPTIONS "$1" | awk '/OPTIONS/ {print $2, $3, $4}'
+curl -sI "$1" | awk '/Allow:/ {print $2, $3, $4}'
