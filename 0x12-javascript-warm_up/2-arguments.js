acb504f1b70b1a6558ca8723f181bad43cf64b01
Yahaya Azeez Adebayo <azconcept2016@gmail.com>
2023-11-13 18:20:30 +0100

    use console.log to print all output :recycle:

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -8,6 +8,8 @@ const args = process.argv;
 
 if (args.length === 2) {
   console.log('No argument');
+} else if (args.length === 3)){
+  console.log("Argument found");
 } else {
-  console.log(`${(args.length === 3) ? "Argument found" : "Arguments found"}`);
+  console.log("Arguments found")
 }
