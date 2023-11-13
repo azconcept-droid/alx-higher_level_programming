da4ff39847d304b1864a5254dc87ec47339952d7
Yahaya Azeez Adebayo <azconcept2016@gmail.com>
2023-11-13 18:27:03 +0100

    refactor back to ternary operator :refactor:

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -8,8 +8,6 @@ const args = process.argv;
 
 if (args.length === 2) {
   console.log('No argument');
-} else if (args.length === 3)){
-  console.log("Argument found");
 } else {
-  console.log("Arguments found")
+  console.log(`${(args.length === 3) ? "Argument found" : "Arguments found"}`);
 }
