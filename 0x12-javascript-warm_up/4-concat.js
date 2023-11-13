b233243b9417a3aa00fa8e9d9e1f89b2ac6fa478
Yahaya Azeez Adebayo <azconcept2016@gmail.com>
2023-11-13 17:58:40 +0100

    refactor code :recycle:

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -6,10 +6,4 @@
 
 const args = process.argv;
 
-if (args.length === 2) {
-  console.log('undefined is undefined');
-} else if (args.length === 3) {
-  console.log(`${args[2]} is undefined`);
-} else {
-  console.log(`${args[2]} is ${args[3]}`);
-}
+console.log(`${args[2]} is ${args[3]}`);
