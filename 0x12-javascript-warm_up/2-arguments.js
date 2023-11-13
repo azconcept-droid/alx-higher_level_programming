2cd319bd9ba0916c5554e64ebdd4982f873e917e
Yahaya Azeez Adebayo <azconcept2016@gmail.com>
2023-11-13 15:27:41 +0100

    fix if statements condition bugs :bug:

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -6,10 +6,10 @@
 
 const args = process.argv;
 
-if (args.length === 2) {
+if (args.length <= 2) {
   console.log('No argument');
 } else if (args.length === 3) {
   console.log('Argument found');
-} else {
+} else if (args.length >= 4) {
   console.log('Argments found');
 }
