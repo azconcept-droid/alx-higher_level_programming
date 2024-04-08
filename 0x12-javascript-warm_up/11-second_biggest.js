fa1d769012de2fae153e13af24386ff611c6b601
Yahaya Azeez <azconcept2016@gmail.com>
2024-04-08 22:13:34 +0100

    feat: fix code semistandard :recycle:

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -10,9 +10,9 @@ if (argsList.length === 2 || argsList.length === 3) {
   console.log(0);
 } else {
   // Slice out 1st two elements
-  let numList = argsList.slice(2);
+  const numList = argsList.slice(2);
   // Sort in descending order
-  let sortedNumbers = numList.sort((a, b) => b - a);
+  const sortedNumbers = numList.sort((a, b) => b - a);
 
-  console.log(sortedNumbers[1])
+  console.log(sortedNumbers[1]);
 }
