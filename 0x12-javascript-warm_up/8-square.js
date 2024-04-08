c9bda959e36d0fb6559f388811785747030993ab
Yahaya Azeez <azconcept2016@gmail.com>
2024-04-08 22:07:20 +0100

    feat: fix code semistandard :recycle:

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,16 +6,16 @@
 
 const inputArgs = process.argv;
 
-const squareSize = parseInt(inputArgs[2])
+const squareSize = parseInt(inputArgs[2]);
 
 if (squareSize) {
-    for (let row = 0; row < squareSize; row++) {
-      let printX = '';
-      for (let col = 0; col < squareSize; col++) {
-        printX += 'X';
-      }
-      console.log(printX);
+  for (let row = 0; row < squareSize; row++) {
+    let printX = '';
+    for (let col = 0; col < squareSize; col++) {
+      printX += 'X';
     }
+    console.log(printX);
+  }
 } else {
-    console.log('Missing size');
+  console.log('Missing size');
 }
