d9893c9cc1e51d406777bd051b22dcb392007e09
Yahaya Azeez <azconcept2016@gmail.com>
2024-04-27 14:47:21 +0100

    fix: comment for documentation :bulb:

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-This script takes in a URL and an email,
-sends a POST request to the passed URL with the email as a parameter
-displays the body of the response(decoded in utf-8)
+Script that takes in a letter and
+sends a POST request to http://0.0.0.0:5000/search_user
 """
 import requests
 import sys
