503807a5ca426127790739c36fa6f4345eb19896
Yahaya Azeez <azconcept2016@gmail.com>
2024-04-27 14:40:26 +0100

    feat: refactor code :recycle:

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,18 +11,16 @@ import sys
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    if len(sys.argv) == 1:
-        q = ""
-        print('No result')
-    else:
-        q = sys.argv[1]
-        letter = {'letter': q}
-        res = requests.post(url, letter)
-    # with urllib.request.post(url, data={'email': sys.argv[2]}) as r:
-    #     print('Your email is: {}'.format(r.getheader('email')))
+    q = "" if len(sys.argv) == 1 else sys.argv[1]
 
-# if __name__ == "__main__":
-#     url = sys.argv[1]
-#     email = {'email': sys.argv[2]}
-#     res = requests.post(url, data=email)
-#     print(res.text)
+    letter = {'q': q}
+    res = requests.post(url, letter)
+
+    try:
+        json = res.json()
+        if json:
+            print('[{}] {}'.format(json.get('id'), json.get('name')))
+        else:
+            print('No result')
+    except Exception:
+        print('Not a valid JSON')
