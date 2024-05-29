d91b809868c3b4d741a3959a833311a24f8142da
Yahaya Azeez <azconcept2016@gmail.com>
2024-05-30 00:53:44 +0100

    add click and enter event listener

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,22 @@
+$(document).ready( function () {
+
+    const code = $('#language_code').val()
+    const translate = $('#btn_translate').val()
+
+    $.ajax({
+        url: 'https://hellosalut.stefanbohacek.dev/?lang=' + code, // api endpoint
+        method: 'GET',
+        success: function(data) {
+
+            $('#btn_translate').on('click enter', function ( event ) {
+                $('#hello').text(data.hello);
+              });
+
+            console.log(data)
+        },
+        error: function(error) {
+            $('#hello').text("Can't fetch data, check your internet connection");
+            console.error('Error fetching data:', error);
+        }
+    });
+})
